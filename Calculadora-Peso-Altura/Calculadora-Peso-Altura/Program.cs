78fc7590ad7481d3fe9f982f90acf92330a4583f
André <andresouza.rib2@gmail.com>
2021-05-06 15:43:13 -0300

    Calculadora-Peso-Altura

---
+++ Calculadora-Peso-Altura/Calculadora-Peso-Altura/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_Peso_Altura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char escolha;
+            double altura, pesoIdeal;
+
+            Console.WriteLine("Escolha seu sexo (M)|(F)");
+            escolha = char.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua altura: ");
+            altura = double.Parse(Console.ReadLine());
+
+            if (escolha == 'M' || escolha == 'm')
+            {
+                pesoIdeal = (72.7 * altura) - 58;
+                Console.WriteLine($@"Seu peso ideal é {pesoIdeal.ToString("N2")}
+Programa Finalizado");
+            }
+            else if (escolha == 'F' || escolha == 'f')
+            {
+                pesoIdeal = (62.1 * altura) - 44.7;
+                Console.WriteLine($@"Seu peso ideal é {pesoIdeal.ToString("N2")}
+Programa Finalizado");
+            }
+            else
+            {
+                Console.WriteLine($"{altura} não é um sexo válido.");
+            }
+        }
+    }
+}
