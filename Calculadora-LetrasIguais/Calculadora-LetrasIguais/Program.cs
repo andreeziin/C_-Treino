78e7e704352ead7519630e8164b8baaf5bc8c2d1
André <andresouza.rib2@gmail.com>
2021-05-10 21:47:52 -0300

    Calculadora-LetrasIguais

---
+++ Calculadora-LetrasIguais/Calculadora-LetrasIguais/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_LetrasIguais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numeroUm, numeroDois, soma, subtracao;
+
+            Console.WriteLine("Programa Iniciado.");
+
+            Console.WriteLine("\nDigite dois números iguais ou diferentes.");
+
+            Console.Write("\nDigite o número um: ");
+            numeroUm = double.Parse(Console.ReadLine());
+
+            Console.Write("\nDigite o número dois: ");
+            numeroDois = double.Parse(Console.ReadLine());
+
+            if (numeroUm == numeroDois)
+            {
+                soma = numeroUm + numeroDois;
+                Console.WriteLine($"\nA soma dos dois números iguais é: {soma}.");
+
+                Console.WriteLine("\nPrograma Finalizado");
+            }
+            else if(numeroUm != numeroDois)
+            {
+                subtracao = numeroUm - numeroDois;
+                Console.WriteLine($"\nA subtração dos números diferentes é: {subtracao}.");
+
+                Console.WriteLine("\nPrograma Finalizado.");
+            }
+        }
+    }
+}
